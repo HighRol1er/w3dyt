c33f2205683eb2bb856597e95a871f006350a925
HighRol1er <hunterjoe9999@gmail.com>
2025-02-24 15:15:13 +0900

    refactor: upbit-manager.service.ts 불필요하해서 upbit.module로 합침

--- server/src/collector/exchange/upbit/upbit.module.ts
+++ server/src/collector/exchange/upbit/upbit.module.ts
@@ -1,11 +1,31 @@
-import { Module } from '@nestjs/common';
+// import { Module } from '@nestjs/common';
+// import { RedisService } from 'src/redis/redis.service';
+// import { UpbitHttpService } from './upbit-http.service';
+// import { UpbitWebSocketService } from './upbit-ws.service';
+// import { UpbitService } from './upbit-manager.service';
+// @Module({
+//   imports: [],
+//   providers: [UpbitWebSocketService, UpbitHttpService, UpbitService, RedisService],
+//   exports: [UpbitService, UpbitHttpService],
+// })
+// export class UpbitModule {}
+import { Module, OnModuleInit } from '@nestjs/common';
 import { RedisService } from 'src/redis/redis.service';
 import { UpbitHttpService } from './upbit-http.service';
 import { UpbitWebSocketService } from './upbit-ws.service';
-import { UpbitService } from './upbit-manager.service';
+
 @Module({
-  imports: [],
-  providers: [UpbitWebSocketService, UpbitHttpService, UpbitService, RedisService],
-  exports: [UpbitService, UpbitHttpService],
+  providers: [UpbitHttpService, UpbitWebSocketService, RedisService],
+  exports: [UpbitHttpService, UpbitWebSocketService],
 })
-export class UpbitModule {}
+export class UpbitModule implements OnModuleInit {
+  constructor(
+    private readonly upbitHttpService: UpbitHttpService,
+    private readonly upbitWebSocketService: UpbitWebSocketService,
+  ) {}
+
+  async onModuleInit() {
+    await this.upbitHttpService.fetchAllMarketData();
+    await this.upbitWebSocketService.connectWebSocket();
+  }
+}
